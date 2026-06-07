85ed51f054e20fcc44c8132a00aa6511b8ff529e
nightusk <nightusk@gmail.com>
2026-06-07 19:26:31 +0900

    Dynamic Library

--- build.zig
+++ build.zig
@@ -6,7 +6,8 @@ pub fn build(b: *std.Build) void {
 
     const libfizzbuzz = b.addLibrary(.{
         .name = "fizzbuzz",
-        .linkage = .static,
+        .linkage = .dynamic,
+        .version = .{ .major = 1, .minor = 2, .patch = 3 },
         .root_module = b.createModule(.{
             .root_source_file = b.path("fizzbuzz.zig"),
             .target = target,
