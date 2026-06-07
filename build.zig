d03aa66e9b3251854893184d6129574c44dea150
nightusk <nightusk@gmail.com>
2026-06-07 14:47:07 +0900

    Standard Configuration Options

--- build.zig
+++ build.zig
@@ -1,14 +1,14 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const windows = b.option(bool, "windows", "Target Microsoft Windows") orelse false;
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
         .name = "hello",
         .root_module = b.createModule(.{
             .root_source_file = b.path("hello.zig"),
-            .target = b.resolveTargetQuery(.{
-                .os_tag = if (windows) .windows else null,
-            }),
+            .target = target,
+            .optimize = optimize,
         }),
     });
     b.installArtifact(exe);
