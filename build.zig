e9e80a143bb04f370631177462f20dd965a062aa
nightusk <nightusk@gmail.com>
2026-06-07 14:47:42 +0900

    Options for Conditional Compilation

--- build.zig
+++ build.zig
@@ -4,16 +4,28 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
-        .name = "hello",
+        .name = "app",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("hello.zig"),
+            .root_source_file = b.path("app.zig"),
             .target = target,
             .optimize = optimize,
         }),
     });
+
+    const version = b.option([]const u8, "version", "application version string") orelse "0.0.0";
+    const enable_foo = detectWhetherToEnableLibFoo();
+
+    const options = b.addOptions();
+    options.addOption([]const u8, "version", version);
+    options.addOption(bool, "have_libfoo", enable_foo);
+    exe.root_module.addOptions("config", options);
+
     b.installArtifact(exe);
 
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
+}
+fn detectWhetherToEnableLibFoo() bool {
+    return false;
 }
